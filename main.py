3a28b0bed9387d54171038f54b993dd24183055d
NaWell55 <melihkuku.5561@gmail.com>
2026-01-16 20:56:50 +0300

    main.py

--- main.py
+++ main.py
@@ -1,16 +1,73 @@
-# This is a sample Python script.
+import math
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+isim = input("İsminizi Girin: ")
+yas = int(input("Yaşınızı Girin: "))
+if (yas > 90) and (yas < 9):
+    print("Lütfen gerçek yaşınızı giriniz")
+elif (yas < 90) and (yas > 9):
+    print("Hoşgeldin " + isim.title() + " " + "Yaşın: " + str(yas))
+    while True:
+        print("""\n--- MENÜ ---
+        1- Yapılacak ekle
+        2- Yapılacak Listesi
+        3- Yapılacak Sil
+        4- Hesap makinesi 
+        5- Çıkış""")
+
+        secim = input("Seçiminizi girin (1-6): ")
+
+        if secim == "1":
+            print("""\n--- Yapılıcak Ekle ---""")
+
+            YapilacakEkle = input("Ne eklemek istersiniz: ")
+
+        elif secim == "2":
+            print("""\n--- Yapılacak Listesi ---""")
+            print(str(YapilacakEkle).title())
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+        elif secim == "3":
+            print("""\n--- Yapılacak Sil ---""")
+            YapilacakSil = input(YapilacakEkle.title() + " Adlı Yapılacağınızı silmek istiyor musunuz?(Evet, Hayır): ")
+            if YapilacakSil == "Evet":
+                YapilacakEkle = ""
+                print("Yapılacak silindi.")
 
+        elif secim == "4":
+            print("""\n--- Hesap makinesi ---""")
+            sayi1 = int(input("1. Sayıyı Giriniz: "))
+            sayi2 = int(input("2. Sayıyı Giriniz: "))
+            islemsecim = input("""İşlem Seçiniz
+            (Çıkmak için "Çık" yazın)
+            1-Toplama
+            2-Çıkarma
+            3-Çarpma
+            4-Bölme
+            5-Üssünü Alma
+            6-Karekök Alma
+            Seçiminiz: """)
+            if islemsecim == "1":
+                print(sayi1 + sayi1)
+            elif islemsecim == "2":
+                print(sayi1 - sayi2)
+            elif islemsecim == "3":
+                print(sayi1 * sayi2)
+            elif islemsecim == "4":
+                print(sayi1 / sayi2)
+            elif islemsecim == "5":
+                print(sayi1 ** sayi2)
+            elif islemsecim == "6":
+                karekok = int(input("Karekök almak istediğiniz sayıyı girin: "))
+                print(math.sqrt(karekok))
+            elif islemsecim == "Çık":
+                print()
+            else:
+                print("Yanlış bir rakam yazdınız")
+        elif secim == "5":
+            cikis = input("Çıkmak istediğinze emin misiniz?(Evet, Hayır): ")
+            if cikis == "Evet":
+                print("Güle Güle...")
+                break
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+else:
+    print("Yanlış karakter(ler) kullandınız bir daha deneyiniz...")
